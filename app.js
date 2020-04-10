c2f3c0e4a2088c0870883aaf213b6bb8e5ceb4ae
charlenne alexander <charlennep@gmail.com>
2020-04-10 16:22:01 -0500

    passed all the tests

--- app.js
+++ app.js
@@ -1,35 +1,73 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const Employee = require("./lib/Employee");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
 const render = require("./lib/htmlRenderer");
-​
-​
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+
+const questions = [{
+        type: "input",
+        message: "What is your full name?",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "What is your Employee Id?",
+        name: "id"
+    },
+    {
+        type: "Input",
+        message: "What is you  email address?",
+        name: "email"
+    },
+    {
+        type: "list",
+        message: "Employee type.",
+        choices: ["Manager", "Engineer", "Intern"],
+        name: "role"
+    },
+    {
+        type: "list",
+        message: "Number of Employees",
+        choices: [1, 2, 3, "no more Employees"],
+        name: "employeeNumber"
+    }
+]
+
+function inquireQuestions() {
+    inquirer
+        .prompt(questions)
+        .then(function (response) {
+            if (response.role === "Manager") {
+                inquirer.prompt({
+                    type: "input",
+                    message: "What is your office number?",
+                    name: "officeNum"
+                }).then(function (manager) {
+                    console.log(manager.officeNum);
+                })
+            } else if (response.role === "Engineer") {
+                inquirer.prompt({
+                    type: "input",
+                    message: "What is your GithHub username?",
+                    name: "github"
+                }).then(function (engineer) {
+                    console.log(engineer.github);
+                })
+            } else if (response.role === "Intern") {
+                inquirer.prompt({
+                    type: "input",
+                    message: "What school did you attend?",
+                    name: "school"
+                }).then(function (intern) {
+                    console.log(intern.school);
+                })
+            }
+
+        })
+}
+inquireQuestions()
